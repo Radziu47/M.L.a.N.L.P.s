91162ee3235a5fd6c3f630e9291f4913437ddbfb
Radziu47 <radziu.radziszewski@gmail.com>
2022-06-11 17:44:36 +0200

    first commit

---
+++ Radzio C#/Learning #1/index.cs
@@ -0,0 +1,12 @@
+﻿class index{ 
+    public void Main(string[] args){
+        dynamic num1 = 10;
+        dynamic num2 = 11;
+        dynamic  num3 = 12;
+
+          void Log(){
+        int sum = num1 + num2 + num3;
+        Console.WriteLine(sum);
+        }
+      }
+  }
